b517c32c6093dcae61324018ae280e52f41a2f0e
reljai <reljai@hotmail.com>
2016-05-16 05:57:55 -0700

    more styling

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var { Link } = require('react-router');
 
 var Examples = () => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Examples coming up here</p>
+            <h3 className="text-center">Examples</h3>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to="/?location=Seattle">Seattle, WA</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Los%20Altos">Los Altos, CA</Link>
+                </li>
+            </ol>
         </div>
     );
 };
